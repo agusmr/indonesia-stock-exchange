c801da15a9175c0f5dbc8e054a52f08406cc5ea7
Lukman Edwindra <lukman.edwindra@gmail.com>
2020-01-17 03:01:52 +0700

    Create test_get_company_profile.py

---
+++ test/test_get_company_profile.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestCompanyProfile(unittest.TestCase):
+    
+    def test_something(self):
+        # TO DO
+        # self.assertEqual()
+
+if __name__ == '__main__':
+    unittest.main()
