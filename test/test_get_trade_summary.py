f09755386069a8149873c99159e8f959f2b4a921
Lukman Edwindra <lukman.edwindra@gmail.com>
2020-01-17 03:02:24 +0700

    Create test_get_trade_summary.py

---
+++ test/test_get_trade_summary.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestTradeSummary(unittest.TestCase):
+    
+    def test_something(self):
+        # TO DO
+        # self.assertEqual()
+
+if __name__ == '__main__':
+    unittest.main()
