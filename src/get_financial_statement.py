7e5ff149108d0855ed537be3e65c052d333c33f3
Lukman Edwindra <lukman.edwindra@gmail.com>
2020-01-18 16:23:28 +0700

    Update get_financial_statement.py

--- src/get_financial_statement.py
+++ src/get_financial_statement.py
@@ -1,15 +1,32 @@
 import os
 import requests
+import sys
 from datetime import datetime, timedelta
 
-class CompanyAndPeriod:
+YEAR = sys.argv[1]
+QUARTER = sys.argv[2]
+YEARLY = sys.argv[3]
+
+class FinancialStatement:
+
     def __init__(self, ticker_code, year, quarter, yearly):
+        """
+        Get attributes in order to get URLs.
+
+        Arguments:
+            - ticker_code = Company ID in Indonesia Stock Exchange. 
+            - year = user inputted four digit character. 
+            - quarter = Q1, ..., Q3 should be put as TW1, ..., TW3, and Q4 should be Audit.
+            - yearly = Q1, ..., Q3 should be put as I, ..., III, and Q4 should be Tahunan.
+        """
+
         self.ticker_code = ticker_code
         self.year = year
         self.quarter = quarter
         self.yearly = yearly
 
     def get_url(self):
+
         url = f"http://www.idx.co.id/Portals/0/StaticData/ListedCompanies/Corporate_Actions/New_Info_JSX/Jenis_Informasi/01_Laporan_Keuangan/02_Soft_Copy_Laporan_Keuangan//Laporan%20Keuangan%20Tahun%20{self.year}/{self.quarter}/{self.ticker_code}/FinancialStatement-{self.year}-{self.yearly}-{self.ticker_code}.xlsx"
         response = requests.get(url)
         if response.status_code == 200:
@@ -18,9 +35,9 @@ class CompanyAndPeriod:
 def main():
     listed_company = requests.get('https://www.idx.co.id/umbraco/Surface/Helper/GetEmiten?emitenType=s').json()
     ticker_code = [listed_company[x]['KodeEmiten'] for x in range(len(listed_company))]
-    for i in ticker_code:
-        CompanyAndPeriod(i, 2018, 'Audit', 'Tahunan').get_url()
-
+    financial_statement = [FinancialStatement(x, YEAR, QUARTER, YEARLY) for x in ticker_code]
+    [x.get_url() for x in financial_statement]
+    
 if __name__ == '__main__':
     os.chdir('tmp')
     main()
