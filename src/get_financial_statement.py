0399248f46dc8d6613702e0abe3fb5d2daae92d7
Lukman Edwindra <lukman.edwindra@gmail.com>
2020-01-18 17:27:48 +0700

    Update get_financial_statement.py

--- src/get_financial_statement.py
+++ src/get_financial_statement.py
@@ -1,7 +1,8 @@
 import os
+import pandas as pd
 import requests
 import sys
-from datetime import datetime, timedelta
+from zipfile import BadZipFile
 
 YEAR = sys.argv[1]
 QUARTER = sys.argv[2]
@@ -32,12 +33,51 @@ class FinancialStatement:
         if response.status_code == 200:
             os.system(f'wget {url}')
 
-def main():
+    def get_general_info(self):
+
+        df = pd.read_excel(f'FinancialStatement-{self.year}-{self.yearly}-{self.ticker_code}.xlsx', sheet_name=1)
+        df = df[['Unnamed: 2', 'Unnamed: 1']]
+        df = df.transpose()
+        df = df.rename(columns=df.iloc[0]).iloc[1:].reset_index(drop=True)
+        df = df[[
+            'Entity code',
+            'Current period end date',
+            'Description of presentation currency',
+            'Conversion rate at reporting date if presentation currency is other than rupiah',
+            'Level of rounding used in financial statements',
+            'Type of report on financial statements',
+            'Type of auditor\'s opinion',
+            'Current year auditor'
+        ]]
+        df.columns = [
+            'entity_code',
+            'period',
+            'currency',
+            'conversion_rate',
+            'rounding',
+            'type_of_report',
+            'audit_opinion',
+            'auditor'
+        ]
+
+        return df.to_csv(f'general-info-{self.ticker_code}-{self.year}-{self.yearly}.csv', index=False)
+
+def get_financial_statement():
+
     listed_company = requests.get('https://www.idx.co.id/umbraco/Surface/Helper/GetEmiten?emitenType=s').json()
     ticker_code = [listed_company[x]['KodeEmiten'] for x in range(len(listed_company))]
     financial_statement = [FinancialStatement(x, YEAR, QUARTER, YEARLY) for x in ticker_code]
     [x.get_url() for x in financial_statement]
     
+    for i in range(len(financial_statement)):
+        try:
+            financial_statement[i].get_general_info()
+        except FileNotFoundError:
+            pass
+        except BadZipFile:
+            pass
+
 if __name__ == '__main__':
     os.chdir('tmp')
-    main()
+    get_financial_statement()
+    os.sys
