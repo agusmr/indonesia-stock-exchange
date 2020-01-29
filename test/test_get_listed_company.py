7c4d03fea458f457248845c9e09a411c40e0b460
ledwindra <lukman.edwindra@gmail.com>
2020-01-29 16:55:26 +0700

    added test cases for listed company

--- test/test_get_listed_company.py
+++ test/test_get_listed_company.py
@@ -11,8 +11,40 @@ PSQL_CONNECT = psycopg2.connect(
     user=CONNECTION['user'],
     password=CONNECTION['password']
 )
+CUR = PSQL_CONNECT.cursor()
 
 class TestListedCompany:
-    def test_connection(self):
+
+    def test_psql_connection(self):
         assert PSQL_CONNECT.closed == 0
+
+    def test_cur_connection(self):
+        assert CUR.closed == False
+    
+    def test_column_name(self):
+        CUR.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'listed_company'")
+        column_name = [x[0] for x in CUR.fetchall()]
+        assert [x for x in column_name] == ['ticker_code', 'company_name', 'created_at']
+
+    def test_column_type(self):
+        CUR.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'listed_company'")
+        column_name = [x[1] for x in CUR.fetchall()]
+        assert [x for x in column_name] == ['character varying', 'character varying', 'timestamp without time zone']
+
+    def test_count(self):
+        url = 'https://www.idx.co.id/umbraco/Surface/Helper/GetEmiten?emitenType=s'
+        response = requests.get(url)
+        data = response.json()
+        CUR.execute("SELECT COUNT(*) FROM listed_company")
+        count = CUR.fetchone()[0]
+        assert count == len(data)
+
+    
+    def test_psql_connection(self):
         PSQL_CONNECT.close()
+        assert PSQL_CONNECT.closed == 1
+
+    def test_cur_connection(self):
+        CUR.close()
+        assert CUR.closed == True
+
